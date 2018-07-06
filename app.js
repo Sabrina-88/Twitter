172d3c297427e63343793164ad26b10d3abdfc00
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-07-06 17:11:34 -0300

    VERSÃO 0.0.1

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+var limpar = document.getElementById("oqueEscrevi");
+function Twittar() {
+
+var pegartweet =  document.getElementById("oqueEscrevi").value;
+console.log(pegartweet);//pegar todos os elementos
+var resposta = document.createElement('p');
+	resposta.textContent = pegartweet;
+var postar =   document.getElementById("aparecerAqui");
+postar.appendChild(resposta);
+
+event.preventDefault();
+limpar.value = "";
+}
+ 
