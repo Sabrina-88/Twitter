12323b897b5752517be8648d6e285bdf9254660f
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-07-07 01:10:48 -0300

    VERSÃO 0.0.2

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var limpar = document.getElementById("oqueEscrevi");
-function Twittar() {
+var pegartweet =  document.getElementById("oqueEscrevi");
 
-var pegartweet =  document.getElementById("oqueEscrevi").value;
-console.log(pegartweet);//pegar todos os elementos
+//funcao pegar tweet, imprimir no HTML e limpar textArea
+function Twittar() {
+pegartweet =  document.getElementById("oqueEscrevi").value;
 var resposta = document.createElement('p');
 	resposta.textContent = pegartweet;
 var postar =   document.getElementById("aparecerAqui");
 postar.appendChild(resposta);
-
 event.preventDefault();
 limpar.value = "";
 }
- 
+
+//funcao contar caractere e desativar 
+function caractere(){
+	var max = new Number();
+	max = 10;
+	var atual = document.getElementById('oqueEscrevi').value.length;
+	var contagem = new Number();
+	contagem = max - atual;
+	document.getElementById('restante').innerHTML = contagem;
+	if(contagem >= 0){
+		document.getElementById('botao').disabled="";
+		}else{
+			document.getElementById('botao').disabled="disabled";
+			}
+
+}
