55ad6e2039e8864c378e09c76df28a9538599de3
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-07-07 02:28:16 -0300

    VERSÃO 0.0.3

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ var pegartweet =  document.getElementById("oqueEscrevi");
 
 //funcao pegar tweet, imprimir no HTML e limpar textArea
 function Twittar() {
-pegartweet =  document.getElementById("oqueEscrevi").value;
-var resposta = document.createElement('p');
-	resposta.textContent = pegartweet;
-var postar =   document.getElementById("aparecerAqui");
-postar.appendChild(resposta);
-event.preventDefault();
-limpar.value = "";
+  pegartweet =  document.getElementById("oqueEscrevi").value;
+  var resposta = document.createElement('p');
+  resposta.textContent = pegartweet;
+  var postar =   document.getElementById("aparecerAqui");
+  postar.appendChild(resposta);
+  event.preventDefault();
+  limpar.value = "";
 }
 
-//funcao contar caractere e desativar 
+//funcao contar caractere, desativar botao e alterar cores dos caracteres restantes
 function caractere(){
-	var max = new Number();
-	max = 10;
-	var atual = document.getElementById('oqueEscrevi').value.length;
-	var contagem = new Number();
-	contagem = max - atual;
-	document.getElementById('restante').innerHTML = contagem;
-	if(contagem >= 0){
-		document.getElementById('botao').disabled="";
-		}else{
-			document.getElementById('botao').disabled="disabled";
-			}
+  var max = new Number();
+  max = 140;
+  var atual = document.getElementById('oqueEscrevi').value.length;
+  var contagem = new Number();
+  contagem = max - atual;
+  document.getElementById('restante').innerHTML = contagem;
+  if(contagem >= 0){
+    if (contagem <20 && contagem >10){
+      document.getElementById('restante').style.color='#F1D30A'; 
+      }
+      if (contagem <10 && contagem >=0){
+      document.getElementById('restante').style.color='#E02525'; 
+    }
+    document.getElementById('botao').disabled="";
+    }else{
+      document.getElementById('botao').disabled="disabled";
+	  
+      }
 
 }
